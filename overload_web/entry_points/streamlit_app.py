1fe82970dddc9a8b22c69c9cf2f75a7189070d39
Charlotte Kostelic <charlottekostelic@gmail.com>
2024-12-19 15:36:23 -0500

    fixed paths in streamlit_app.py

--- overload_web/entry_points/streamlit_app.py
+++ overload_web/entry_points/streamlit_app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
 
-home_page = st.Page("tools/home.py", title="Tools")
-get_bib_page = st.Page("tools/get_bib.py", title="Get Bib")
-pvf_page = st.Page("tools/pvf.py", title="Process Vendor File")
-wc2sierra_page = st.Page("tools/wc2sierra.py", title="Worldcat2Sierra")
+home_page = st.Page("home.py", title="Tools")
+get_bib_page = st.Page("get_bib.py", title="Get Bib")
+pvf_page = st.Page("pvf.py", title="Process Vendor File")
+wc2sierra_page = st.Page("wc2sierra.py", title="Worldcat2Sierra")
 
 pg = st.navigation(
     {"Overload Web": [home_page, get_bib_page, pvf_page, wc2sierra_page]}
