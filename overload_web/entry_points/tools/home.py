6e28c7758596302575a40716be98e1455d46a0c8
Charlotte Kostelic <charlottekostelic@gmail.com>
2024-12-19 15:31:22 -0500

    created files for streamlit pages

---
+++ overload_web/entry_points/tools/home.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.markdown(
+    """
+    # Overload Web
+
+    ### A BookOps Cataloging toolbox
+
+    ##### Select a tool below:
+
+     + [Get Bib]()
+     + [Process Vendor File]()
+     + [Worldcat2Sierra]()
+    """
+)
