fa8a32e2a7416fb0a4db20063c6e451639337b27
Charlotte Kostelic <charlottekostelic@gmail.com>
2024-12-26 16:32:13 -0500

    updated static streamlit pages

---
+++ overload_web/front_end/home.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+
+url = "localhost:8501"
+get_bib_url = f"{url}/get_bib"
+vendor_file_url = f"{url}/pvf"
+wc2sierra_url = f"{url}/wc2sierra"
+
+st.markdown(
+    "# Overload Web\n\n"
+    "### A BookOps Cataloging toolbox\n\n"
+    "##### Select a tool below:\n\n"
+    " + [Get Bib]()\n"
+    " + [Process Vendor File]()\n"
+    " + [Worldcat2Sierra]()\n"
+)
--- overload_web/front_end/streamlit_app.py
+++ overload_web/front_end/streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+st.set_page_config(page_title="Overload")
 
 home_page = st.Page("home.py", title="Tools")
 get_bib_page = st.Page("get_bib.py", title="Get Bib")
@@ -9,6 +10,6 @@ wc2sierra_page = st.Page("wc2sierra.py", title="Worldcat2Sierra")
 pg = st.navigation(
     {"Overload Web": [home_page, get_bib_page, pvf_page, wc2sierra_page]}
 )
-st.set_page_config(page_title="Overload")
+
 
 pg.run()
