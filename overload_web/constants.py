8be487ec52a49ed1c7004d8b3ae18286d87b8ac3
Charlotte Kostelic <charlottekostelic@gmail.com>
2025-01-08 17:55:07 -0500

    updated tests and schemas

---
+++ overload_web/constants.py
@@ -0,0 +1,166 @@
+from typing import Any, Dict
+
+APPLICATION: str = "Overload"
+
+LANG: Dict[str, str] = {
+    "ara": "Arabic",
+    "ben": "Bengali",
+    "chi": "Chinese",
+    "eng": "English",
+    "fre": "French",
+    "ger": "German",
+    "heb": "Hebrew",
+    "hin": "Hindi",
+    "hun": "Hungarian",
+    "ita": "Italian",
+    "jpn": "Japanese",
+    "kor": "Korean",
+    "pan": "Panjabi",
+    "pol": "Polish",
+    "por": "Portuguese",
+    "rus": "Russian",
+    "san": "Sanskrit",
+    "spa": "Spanish",
+    "ukr": "Ukrainian",
+    "und": "Undetermined",
+    "urd": "Urdu",
+    "yid": "Yiddish",
+    "zxx": "No linguistic content",
+    "hat": "Haitian French Creole",
+    "alb": "Albanian",
+}
+
+FORMAT: Dict[str, str] = {
+    "-": "undefined",
+    "b": "book",
+    "p": "periodical",
+    "s": "serial",
+    "m": "microform",
+    "c": "score",
+    "r": "audio-other",
+    "u": "map",
+    "t": "videocassette",
+    "f": "film",
+    "d": "cd-rom",
+    "e": "elec access",
+    "k": "kit-bk/audio",
+    "v": "dvd",
+    "w": "music cd",
+    "i": "audio book",
+    "g": "video game",
+    "a": "arch/manusc.",
+    "h": "print & photos",
+    "l": "large print",
+    "j": "realia",
+    "q": "ebook",
+    "z": "ejournal",
+    "y": "bluray",
+    "o": "print & electronic",
+    "x": "playway",
+    "2": "bookpack",
+    "3": "streaming media",
+    "5": "read-along book",
+}
+
+ORDER_TYPE: Dict[str, str] = {
+    "-": "undefined",
+    "f": "firm order",
+    "o": "stand order",
+    "i": "item s.o.",
+    "a": "approval plan",
+    "s": "subscription",
+    "l": "lease",
+    "g": "gift",
+}
+
+SELECTOR: Dict[str, str] = {
+    "-": "---",
+    "1": "Acquisitions",
+    "a": "Jessica",
+    "b": "Stephanie",
+    "c": "Elisa",
+    "d": "Grace",
+    "e": "Emily",
+    "f": "Nick",
+    "g": "Alexandra",
+    "j": "Jason",
+    "l": "Christopher",
+    "m": "Michelle",
+    "n": "Andrew",
+    "r": "Libbhy",
+    "s": "Yolande",
+    "w": "Wayne",
+}
+
+AUDIENCE: Dict[str, str] = {
+    "-": "---",
+    "n": "suppress",
+    "a": "adult",
+    "j": "juvenile",
+    "y": "young adult",
+}
+
+MATCHPOINT: Dict[str, str] = {
+    "isbn": "020",
+    "bib_id": "Sierra ID",
+    "upc": "024",
+    "oclc_number": "001",
+}
+
+TEMPLATE_FIXED_FIELDS: list = [
+    {"name": "Price", "id": "price"},
+    {"name": "Fund", "id": "fund"},
+    {"name": "Copies", "id": "copies"},
+    {"name": "Vendor Code", "id": "vendor_code"},
+    {"name": "Source", "id": "source"},
+    {"name": "Status", "id": "status"},
+    {"name": "Create Date", "id": "create_date"},
+    {
+        "name": "Audience",
+        "id": "audience",
+        "values": AUDIENCE,
+    },
+    {
+        "name": "Order Type",
+        "id": "order_type",
+        "values": ORDER_TYPE,
+    },
+    {
+        "name": "Format",
+        "id": "format",
+        "values": FORMAT,
+    },
+    {
+        "name": "Selector",
+        "id": "selector",
+        "values": SELECTOR,
+    },
+    {
+        "name": "Country",
+        "id": "country",
+        "values": {"xxu": "United States"},
+    },
+    {
+        "name": "Language",
+        "id": "lang",
+        "values": LANG,
+    },
+]
+
+TEMPLATE_VAR_FIELDS: list[dict] = [
+    {"name": "Internal Note", "id": "internal_note"},
+    {"name": "ISBN", "id": "var_field_isbn"},
+    {"name": "Vendor Notes", "id": "vendor_notes"},
+    {"name": "Vendor Title Number", "id": "vendor_title_no"},
+    {"name": "Blanket PO", "id": "blanket_po"},
+]
+
+
+MATCHPOINTS: list[Dict[str, Any]] = [
+    {
+        "id": f"{i}_matchpoint",
+        "name": i.title(),
+        "values": MATCHPOINT,
+    }
+    for i in ["primary", "secondary", "tertiary"]
+]
--- tests/test_api.py
+++ tests/test_api.py
@@ -11,7 +11,7 @@ client = TestClient(app)
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"application": "Overload Web"}
+    assert "BookOps Cataloging Department browser-based toolbox." in response.text
 
 
 @pytest.mark.parametrize("library", ["nypl", "bpl"])
@@ -27,23 +27,23 @@ def test_attach_endpoint(stub_order):
     assert response.json()["bib"]["all_bib_ids"] == ["123456789", "987654321"]
 
 
-@pytest.mark.parametrize("library", ["nypl", "bpl"])
-def test_process_vendor_file_endpoint(stub_order, stub_template, stub_sierra_service):
-    template_schema = schemas.OrderTemplateModel(**asdict(stub_template)).model_dump()
-    order_schema = schemas.OrderModel(**asdict(stub_order)).model_dump()
-    data = {"order": order_schema, "template": template_schema}
-    response = client.post("/vendor_file", json=data)
-    assert response.status_code == 200
-    assert response.url == f"{client.base_url}/vendor_file"
-    assert response.json()["bib"]["fund"] == "10001adbk" == template_schema["fund"]
-    assert response.json()["bib"]["fund"] != order_schema["fund"]
+# @pytest.mark.parametrize("library", ["nypl", "bpl"])
+# def test_process_vendor_file_endpoint(stub_order, stub_template, stub_sierra_service):
+#     template_schema = schemas.OrderTemplateModel(**asdict(stub_template)).model_dump()
+#     order_schema = schemas.OrderModel(**asdict(stub_order)).model_dump()
+#     data = {"order": order_schema, "template": template_schema}
+#     response = client.post("/vendor_file", json=data)
+#     assert response.status_code == 200
+#     assert response.url == f"{client.base_url}/vendor_file"
+#     assert response.json()["bib"]["fund"] == "10001adbk" == template_schema["fund"]
+#     assert response.json()["bib"]["fund"] != order_schema["fund"]
 
 
-@pytest.mark.parametrize("library", ["foo"])
-def test_process_vendor_file_endpoint_invalid_config(stub_order, stub_template):
-    template_schema = schemas.OrderTemplateModel(**asdict(stub_template)).model_dump()
-    order_schema = schemas.OrderModel(**asdict(stub_order)).model_dump()
-    data = {"order": order_schema, "template": template_schema}
-    with pytest.raises(ValueError) as exc:
-        client.post("/vendor_file", json=data)
-    assert "Invalid library. Must be 'bpl' or 'nypl'" in str(exc.value)
+# @pytest.mark.parametrize("library", ["foo"])
+# def test_process_vendor_file_endpoint_invalid_config(stub_order, stub_template):
+#     template_schema = schemas.OrderTemplateModel(**asdict(stub_template)).model_dump()
+#     order_schema = schemas.OrderModel(**asdict(stub_order)).model_dump()
+#     data = {"order": order_schema, "template": template_schema}
+#     with pytest.raises(ValueError) as exc:
+#         client.post("/vendor_file", json=data)
+#     assert "Invalid library. Must be 'bpl' or 'nypl'" in str(exc.value)
