5bce97980911bb5505c833f8e267450ae46ab105
Charlotte Kostelic <charlottekostelic@gmail.com>
2025-01-15 12:06:25 -0500

    reduced duplication b/t pydantic and domain models

--- overload_web/adapters/schemas.py
+++ overload_web/adapters/schemas.py
@@ -1,9 +1,18 @@
 from __future__ import annotations
 import datetime
 import json
-from typing import Annotated, List, Optional, Union
+from typing import Annotated, Optional, Union
 from fastapi import Form, UploadFile
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
+from overload_web.domain import model
+
+
+class OrderModel(BaseModel, model.Order):
+    model_config = ConfigDict(from_attributes=True)
+
+
+class OrderTemplateModel(BaseModel, model.OrderTemplate):
+    model_config = ConfigDict(from_attributes=True)
 
 
 def get_template(
@@ -58,55 +67,3 @@ def get_order_file(
     order_data = order_file.file
     order = json.load(order_data)
     return OrderModel(**order)
-
-
-class OrderModel(BaseModel):
-    library: str
-    create_date: Union[datetime.datetime, str]
-    locations: List[str]
-    shelves: List[str]
-    price: Union[str, int]
-    fund: str
-    copies: Union[str, int]
-    lang: str
-    country: str
-    vendor_code: str
-    format: str
-    selector: str
-    audience: str
-    source: str
-    order_type: str
-    status: str
-    internal_note: Optional[str]
-    var_field_isbn: Optional[str]
-    vendor_notes: Optional[str]
-    vendor_title_no: Optional[str]
-    blanket_po: Optional[str]
-    bib_id: Optional[Union[str, int]]
-    upc: Optional[Union[str, int]]
-    isbn: Optional[Union[str, int]]
-    oclc_number: Optional[Union[str, int]]
-
-
-class OrderTemplateModel(BaseModel):
-    create_date: Optional[Union[datetime.datetime, str]] = None
-    price: Optional[Union[str, int]] = None
-    fund: Optional[str] = None
-    copies: Optional[Union[str, int]] = None
-    lang: Optional[str] = None
-    country: Optional[str] = None
-    vendor_code: Optional[str] = None
-    format: Optional[str] = None
-    selector: Optional[str] = None
-    audience: Optional[str] = None
-    source: Optional[str] = None
-    order_type: Optional[str] = None
-    status: Optional[str] = None
-    internal_note: Optional[str] = None
-    var_field_isbn: Optional[str] = None
-    vendor_notes: Optional[str] = None
-    vendor_title_no: Optional[str] = None
-    blanket_po: Optional[str] = None
-    primary_matchpoint: Optional[str] = None
-    secondary_matchpoint: Optional[str] = None
-    tertiary_matchpoint: Optional[str] = None
