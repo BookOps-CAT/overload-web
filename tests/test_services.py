043cfc8206d86196e6c471c91dba68fe828cb044
Charlotte Kostelic <charlottekostelic@gmail.com>
2025-03-26 10:53:32 -0400

    updated handlers, removed api tests

--- tests/test_services.py
+++ tests/test_services.py
@@ -5,54 +5,64 @@ from overload_web.services import handlers
 
 
 @pytest.mark.parametrize("library", ["nypl", "bpl"])
-def test_apply_template(stub_order, stub_template):
-    bib = model.OrderBib(order=stub_order)
-    assert bib.fund == "25240adbk"
-    new_bib = handlers.apply_template(bib, stub_template)
-    assert new_bib.fund == "10001adbk"
+def test_apply_template(stub_orderbib, stub_template):
+    original_fund = stub_orderbib.orders[0].fund
+    new_bib = handlers.apply_template(bib=stub_orderbib, template=stub_template)
+    assert new_bib.orders[0].fund == "10001adbk"
+    assert original_fund == "25240adbk"
 
 
 @pytest.mark.parametrize("library", ["nypl", "bpl"])
 @pytest.mark.parametrize("all_bibs, bib_id", [(["123456789"], "123456789"), ([], None)])
-def test_attach(stub_order, all_bibs, bib_id):
-    bib = handlers.attach(stub_order, all_bibs)
+def test_attach(stub_order, all_bibs, bib_id, library):
+    bib = handlers.attach(library=library, order_data=stub_order, bib_ids=all_bibs)
     assert isinstance(bib, model.OrderBib)
     assert bib.all_bib_ids == all_bibs
     assert bib.bib_id == bib_id
-    assert stub_order.bib_id is None
 
 
 @pytest.mark.parametrize("library", ["nypl", "bpl"])
-def test_process_file(stub_template, stub_order, stub_sierra_service):
-    stub_order.isbn = "9781234567890"
+def test_process_file(stub_template, stub_orderbib, stub_sierra_service):
+    stub_orderbib.isbn = "9781234567890"
+    original_fund = stub_orderbib.orders[0].fund
     processed_bib = handlers.process_file(
-        stub_sierra_service, stub_order, stub_template
+        sierra_service=stub_sierra_service,
+        order_bib=stub_orderbib,
+        template=stub_template,
     )
-    assert stub_order.fund == "25240adbk"
-    assert stub_order.bib_id is None
-    assert processed_bib.fund == stub_template.fund == "10001adbk"
+
+    assert processed_bib.orders[0].fund == stub_template.fund == "10001adbk"
+    assert original_fund != processed_bib.orders[0].fund
+    assert original_fund != stub_template.fund
     assert processed_bib.bib_id == "123456789"
 
 
 @pytest.mark.parametrize("library", ["nypl", "bpl"])
-def test_process_file_no_matchpoints(stub_template, stub_order, stub_sierra_service):
-    stub_template.primary_matchpoint = None
+def test_process_file_no_matchpoints(stub_template, stub_orderbib, stub_sierra_service):
+    stub_orderbib.orders[0].primary_matchpoint = None
+    original_fund = stub_orderbib.orders[0].fund
     processed_bib = handlers.process_file(
-        stub_sierra_service, stub_order, stub_template
+        sierra_service=stub_sierra_service,
+        order_bib=stub_orderbib,
+        template=stub_template,
     )
-    assert stub_order.bib_id is None
-    assert processed_bib.fund == stub_template.fund == "10001adbk"
-    assert processed_bib.fund != stub_order.fund
+    assert processed_bib.orders[0].fund == stub_template.fund == "10001adbk"
+    assert original_fund != processed_bib.orders[0].fund
+    assert original_fund != stub_template.fund
     assert processed_bib.bib_id is None
 
 
 @pytest.mark.parametrize("library", ["nypl", "bpl"])
-def test_process_file_no_match(stub_template, stub_order, stub_sierra_service):
-    stub_order.isbn = None
+def test_process_file_no_match(stub_template, stub_orderbib, stub_sierra_service):
+    stub_orderbib.orders[0].isbn = None
+    original_fund = stub_orderbib.orders[0].fund
     processed_bib = handlers.process_file(
-        stub_sierra_service, stub_order, stub_template
+        sierra_service=stub_sierra_service,
+        order_bib=stub_orderbib,
+        template=stub_template,
     )
-    assert stub_order.bib_id is None
-    assert processed_bib.fund == stub_template.fund == "10001adbk"
-    assert processed_bib.fund != stub_order.fund
+
+    assert processed_bib.orders[0].fund == stub_template.fund == "10001adbk"
+    assert original_fund != processed_bib.orders[0].fund
+    assert original_fund != stub_template.fund
     assert processed_bib.bib_id is None
