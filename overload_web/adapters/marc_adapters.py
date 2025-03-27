47fd8639e74cca71fc96eb4381650db678183fd0
Charlotte Kostelic <charlottekostelic@gmail.com>
2025-03-27 13:01:51 -0400

    added OverloadBib class, read_marc_file function

--- overload_web/adapters/marc_adapters.py
+++ overload_web/adapters/marc_adapters.py
@@ -1,10 +1,33 @@
 from __future__ import annotations
 
-from typing import List, Optional
+from typing import BinaryIO, Generator, Optional, Sequence
 
-from bookops_marc import Bib
+from bookops_marc import Bib, SierraBibReader
 from bookops_marc.models import Field
 from bookops_marc.models import Order as BookopsMarcOrder
+
+
+class OverloadBib(Bib):
+
+    @property
+    def orders(self) -> Sequence[OverloadOrder]:
+        orders = []
+        for field in self.fields:
+            if field.tag == "960":
+                try:
+                    following_field = self.fields[self.pos]
+                except IndexError:
+                    following_field = None
+                orders.append(OverloadOrder(field, following_field))
+        return orders
+
+    @classmethod
+    def from_bookops_bib(cls, bib: Bib) -> OverloadBib:
+        bib_attrs = {"leader": bib.leader, "fields": bib.fields, "library": bib.library}
+        overload_bib = OverloadBib()
+        for k, v in bib_attrs.items():
+            setattr(overload_bib, k, v)
+        return overload_bib
 
 
 class OverloadOrder(BookopsMarcOrder):
@@ -74,14 +97,10 @@ class OverloadOrder(BookopsMarcOrder):
     def vendor_title_no(self) -> Optional[str]:
         return self._get_subfield_from_following_field("i")
 
-    @staticmethod
-    def orders_from_bib(record: Bib) -> List[OverloadOrder]:
-        orders: List[OverloadOrder] = []
-        for field in record:
-            if field.tag == "960":
-                try:
-                    following_field = record.fields[record.pos]
-                except IndexError:
-                    following_field = None
-                orders.append(OverloadOrder(field, following_field))
-        return orders
+
+def read_marc_file(
+    marc_file: BinaryIO, library: str
+) -> Generator[OverloadBib, None, None]:
+    reader = SierraBibReader(marc_file, library=library, hide_utf8_warnings=True)
+    for record in reader:
+        yield OverloadBib.from_bookops_bib(record)
