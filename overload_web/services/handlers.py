c2e33cc5c1d701bc18c661596c4f323a0f0d4dc8
Charlotte Kostelic <charlottekostelic@gmail.com>
2025-04-10 15:47:14 -0400

    added match_bib, refactored process_file

--- overload_web/services/handlers.py
+++ overload_web/services/handlers.py
@@ -6,6 +6,22 @@ from overload_web.adapters import object_factories, sierra_adapters
 from overload_web.domain import model
 
 
+def match_bib(
+    bib: model.DomainBib,
+    matchpoints: List[str],
+    sierra_service: sierra_adapters.SierraService,
+) -> model.DomainBib:
+    bib_factory = object_factories.BibFactory()
+    bibs = []
+    for key in matchpoints:
+        sierra_bibs = sierra_service.get_bibs_by_id(bib=bib, key=key)
+        bibs.extend(
+            [bib_factory.to_domain(i, library=bib.library) for i in sierra_bibs]
+        )
+    bib.match(bibs=bibs, matchpoints=matchpoints)
+    return bib
+
+
 def process_file(
     bib: model.DomainBib,
     sierra_service: sierra_adapters.SierraService,
@@ -16,12 +32,4 @@ def process_file(
         for order in bib.orders:
             order.apply_template(template=template)
         matchpoints = template.matchpoints
-    bib_factory = object_factories.BibFactory()
-    bibs = []
-    for key in matchpoints:
-        sierra_bibs = sierra_service.get_bibs_by_id(bib=bib, key=key)
-        bibs.extend(
-            [bib_factory.to_domain(i, library=bib.library) for i in sierra_bibs]
-        )
-    bib.match(bibs=bibs, matchpoints=matchpoints)
-    return bib
+    return match_bib(bib=bib, matchpoints=matchpoints, sierra_service=sierra_service)
--- tests/test_services.py
+++ tests/test_services.py
@@ -4,14 +4,6 @@ from overload_web.services import handlers
 
 
 @pytest.mark.parametrize("library", ["nypl", "bpl"])
-def test_apply_template(stub_domain_bib, stub_template):
-    original_fund = stub_domain_bib.orders[0].fund
-    new_bib = handlers.apply_template(bib=stub_domain_bib, template=stub_template)
-    assert new_bib.orders[0].fund == "10001adbk"
-    assert original_fund == "25240adbk"
-
-
-@pytest.mark.parametrize("library", ["nypl", "bpl"])
 def test_process_file(stub_template, stub_domain_bib, stub_sierra_service):
     stub_domain_bib.isbn = "9781234567890"
     original_fund = stub_domain_bib.orders[0].fund
