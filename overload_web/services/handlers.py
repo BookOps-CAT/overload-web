b0126d6574a05e83ce4fcb4be9f146ab2f7e2a5e
Charlotte Kostelic <charlottekostelic@gmail.com>
2025-04-10 15:46:24 -0400

    removed apply_template, changed process_file

--- overload_web/services/handlers.py
+++ overload_web/services/handlers.py
@@ -1,21 +1,27 @@
 from __future__ import annotations
 
-from overload_web.adapters import sierra_adapters
+from typing import List, Optional
+
+from overload_web.adapters import object_factories, sierra_adapters
 from overload_web.domain import model
-
-
-def apply_template(bib: model.DomainBib, template: model.Template) -> model.DomainBib:
-    bib.apply_template(template=template)
-    return bib
 
 
 def process_file(
     bib: model.DomainBib,
     sierra_service: sierra_adapters.SierraService,
-    template: model.Template,
+    template: Optional[model.Template] = None,
+    matchpoints: List[str] = [],
 ) -> model.DomainBib:
-    bib.attach(
-        sierra_service.get_all_bib_ids(order_bib=bib, match_keys=template.matchpoints)
-    )
-    bib.apply_template(template=template)
+    if template:
+        for order in bib.orders:
+            order.apply_template(template=template)
+        matchpoints = template.matchpoints
+    bib_factory = object_factories.BibFactory()
+    bibs = []
+    for key in matchpoints:
+        sierra_bibs = sierra_service.get_bibs_by_id(bib=bib, key=key)
+        bibs.extend(
+            [bib_factory.to_domain(i, library=bib.library) for i in sierra_bibs]
+        )
+    bib.match(bibs=bibs, matchpoints=matchpoints)
     return bib
