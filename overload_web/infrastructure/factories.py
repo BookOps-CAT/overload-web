fcec68b505ea6c82d5b6bb70c079cbc1178501f9
Charlotte Kostelic <charlottekostelic@gmail.com>
2025-06-04 16:43:46 -0400

    added factories for matcher and parser

---
+++ overload_web/infrastructure/factories.py
@@ -0,0 +1,20 @@
+from overload_web.domain.logic import bib_matcher
+from overload_web.infrastructure.bibs import marc_adapters, sierra
+
+
+def get_matcher_for_library(
+    library: str, matchpoints: list
+) -> bib_matcher.BibMatchService:
+    session: sierra.SierraSessionProtocol
+    if library == "bpl":
+        session = sierra.BPLSolrSession()
+    elif library == "nypl":
+        session = sierra.NYPLPlatformSession()
+    else:
+        raise ValueError("Invalid library. Must be 'bpl' or 'nypl'")
+    fetcher = sierra.SierraBibFetcher(session=session, library=library)
+    return bib_matcher.BibMatchService(fetcher=fetcher, matchpoints=matchpoints)
+
+
+def get_parser_for_library(library: str) -> marc_adapters.BookopsMarcTransformer:
+    return marc_adapters.BookopsMarcTransformer(library=library)
