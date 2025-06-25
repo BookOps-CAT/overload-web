95144eee0208a36336d07ede7f5968ab2ce1eda7
Charlotte Kostelic <charlottekostelic@gmail.com>
2025-06-25 16:44:05 -0400

    removed unneeded factory function

--- overload_web/infrastructure/factories.py
+++ overload_web/infrastructure/factories.py
@@ -1,27 +1,11 @@
 import os
-from typing import Any, Optional
+from typing import Optional
 
 from file_retriever import Client
 
-from overload_web.application import dto, services
-from overload_web.domain import logic, models, protocols
-from overload_web.infrastructure.bibs import marc, sierra
+from overload_web.application import services
+from overload_web.domain import protocols
 from overload_web.infrastructure.file_io import local, sftp
-
-
-def get_record_processor(
-    library: str,
-    matchpoints: list[str],
-    template: models.templates.Template | dict[str, Any],
-) -> services.records.RecordProcessingService:
-    fetcher: protocols.bibs.BibFetcher = sierra.SierraBibFetcher(library=library)
-    parser: protocols.bibs.MarcTransformer[dto.bib.BibDTO] = (
-        marc.BookopsMarcTransformer(library=models.bibs.LibrarySystem(library))
-    )
-    matcher = logic.bibs.BibMatcher(fetcher=fetcher, matchpoints=matchpoints)
-    return services.records.RecordProcessingService(
-        matcher=matcher, parser=parser, template=template
-    )
 
 
 def create_file_service(
