d6dca2576378d25ef5871ef85f8cdad3fa6d7b8b
Charlotte Kostelic <charlottekostelic@gmail.com>
2025-07-02 14:52:02 -0400

    renamed jinja_frontend and overload_api modules

--- overload_web/main.py
+++ overload_web/main.py
@@ -8,12 +8,12 @@ from __future__ import annotations
 from fastapi import FastAPI
 
 from overload_web import logging_config
-from overload_web.presentation import jinja_frontend, overload_api
+from overload_web.presentation import api, frontend
 
 logging_config.setup_logging()
 
 app = FastAPI()
 
 
-app.include_router(jinja_frontend.frontend_router)
-app.include_router(overload_api.api_router)
+app.include_router(frontend.frontend_router)
+app.include_router(api.api_router)
