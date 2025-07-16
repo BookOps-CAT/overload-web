c9d80201466c391df58b31f08eccd4eefc06bb1d
Charlotte Kostelic <charlottekostelic@gmail.com>
2025-07-16 16:58:13 -0400

    udpated dependency functions

--- overload_web/presentation/depends_funcs.py
+++ overload_web/presentation/depends_funcs.py
@@ -1,6 +1,6 @@
 import json
 import os
-from typing import Annotated, Optional, Union
+from typing import Annotated, Union
 
 from fastapi import Form, UploadFile
 
@@ -17,18 +17,37 @@ def get_context_form_fields() -> dict[str, dict[str, str | dict[str, str]]]:
 def normalize_files(
     source: Annotated[str, Form(...)],
     files: Annotated[Union[list[UploadFile], list[str]], Form(...)],
-    vendor: Annotated[Optional[str], Form(...)] = None,
+    vendor: Annotated[str, Form(...)],
 ) -> list[schemas.VendorFileModel]:
-    if source == "remote":
-        if not vendor:
-            raise ValueError("Vendor must be provided for remote files")
+    if source == "remote" and not vendor:
+        raise ValueError("Vendor must be provided for remote files")
+
+    file_models = []
+
+    remote_files = []
+    local_files = []
+    for file in files:
+        if isinstance(file, UploadFile):
+            local_files.append(file)
+        else:
+            remote_files.append(file)
+
+    if local_files:
+        file_models.extend(
+            [
+                schemas.VendorFileModel.create(
+                    file_name=f.filename, content=f.file.read()
+                )
+                for f in local_files
+            ]
+        )
+    if remote_files and vendor:
         vendor_dir = os.environ[f"{vendor.upper()}_SRC"]
         service = factories.create_remote_file_service(vendor)
-        loaded_files = [service.loader.load(name=f, dir=vendor_dir) for f in files]
-        return [schemas.VendorFileModel(**f.__dict__) for f in loaded_files]
-    else:
-        file_models = [
-            schemas.VendorFileModel.create(file_name=f.filename, content=f.file.read())
-            for f in files
+        loaded_files = [
+            service.loader.load(name=f, dir=vendor_dir) for f in remote_files
         ]
-        return file_models
+        file_models.extend(
+            [schemas.VendorFileModel(**f.__dict__) for f in loaded_files]
+        )
+    return file_models
