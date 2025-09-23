076e48d280399dee39095b9d11fc5bdeed164114
Charlotte Kostelic <charlottekostelic@gmail.com>
2025-09-23 16:13:14 -0400

    updated docstrings

--- overload_web/infrastructure/bibs/vendor.py
+++ overload_web/infrastructure/bibs/vendor.py
@@ -1,3 +1,5 @@
+"""Parsers to identify vendor information within MARC records"""
+
 from __future__ import annotations
 
 import logging
@@ -11,17 +13,42 @@ logger = logging.getLogger(__name__)
 
 
 class VendorIdentifier:
+    """Parses MARC records to identify the records' vendors based on a set of rules."""
+
     def __init__(
         self,
         vendor_tags: dict[str, dict[str, dict[str, dict[str, str]]]],
         vendor_info: dict[str, Any],
     ) -> None:
+        """
+        Initialize `VendorIdentifier` using a specific set of marc mapping rules.
+
+        Args:
+            vendor_tags:
+                A dictionary a list of vendors and tags that are present in their
+                records.
+            vendor_info:
+                A dictionary containing information about matchpoints to use and fields
+                to add to records for vendors.
+        """
         self.vendor_tags = vendor_tags
         self.vendor_info = vendor_info
 
     def _get_tag_from_bib(
         self, record: Bib, tags: dict[str, dict[str, str]]
-    ) -> dict[str, str]:
+    ) -> dict[str, dict[str, str]]:
+        """
+        Get the MARC tag, subfield code, and value from a record based on a dictionary
+        containing tags and subfield codes that would be present in a vendor's records
+
+        Args:
+            record: A bookops_marc.Bib object
+            tags: A dictionary containing MARC tags, subfield codes, and subfield values
+
+        Returns:
+            A dictionary containing the values present in the MARC fields/subfields.
+
+        """
         bib_dict: dict = {}
         for tag, data in tags.items():
             field = record.get(tag)
@@ -31,7 +58,8 @@ class VendorIdentifier:
                 bib_dict[tag] = {"code": data["code"], "value": field.get(data["code"])}
         return bib_dict
 
-    def vendor_id(self, record: Bib) -> models.bibs.VendorInfo:
+    def identify_vendor(self, record: Bib) -> models.bibs.VendorInfo:
+        """Identify the vendor to whom a `bookops_marc.Bib` record belongs."""
         for vendor, info in self.vendor_tags.items():
             tags: dict[str, dict[str, str]] = info.get("primary", {})
             tag_match = self._get_tag_from_bib(record=record, tags=tags)
