ed4c57f673af6d356318d08f43b7febd36193c16
Charlotte Kostelic <charlottekostelic@gmail.com>
2026-04-14 15:38:22 -0400

    simplified logging config

--- overload_web/main.py
+++ overload_web/main.py
@@ -5,16 +5,19 @@ Initializes the `FastAPI` app and registers backend and frontend routers.
 
 from __future__ import annotations
 
+import json
 import logging
+import logging.config
 from contextlib import asynccontextmanager
+from functools import lru_cache
 from pathlib import Path
-from typing import AsyncGenerator
+from typing import Any, AsyncGenerator
 
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
-from overload_web import config
 from overload_web.presentation import api, deps, frontend, partials
 
 BASE_DIR = Path(__file__).resolve().parent
@@ -23,7 +26,14 @@ load_dotenv(dotenv_path="debug.env")
 logger = logging.getLogger("overload_web")
 
 
-config.setup_logging()
+@lru_cache
+def get_log_config() -> dict[str, Any]:
+    with open(BASE_DIR / "data/logging_config.json", "r", encoding="utf-8") as fh:
+        constants = json.load(fh)
+    return constants
+
+
+logging.config.dictConfig(get_log_config())
 
 
 @asynccontextmanager
@@ -37,9 +47,25 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
     engine.dispose()
 
 
+@lru_cache
+def get_templates() -> Jinja2Templates:
+    """Loads Jinja2 templates and sets env vars"""
+    with open(BASE_DIR / "data/form_constants.json", "r", encoding="utf-8") as fh:
+        constants = json.load(fh)
+    templates = Jinja2Templates(directory=BASE_DIR / "templates")
+    templates.env.globals["fixed_fields"] = constants["fixed_fields"]
+    templates.env.globals["var_fields"] = constants["var_fields"]
+    templates.env.globals["matchpoints"] = constants["matchpoints"]
+    templates.env.globals["bib_formats"] = constants["material_form"]
+    templates.env.globals["context_fields"] = constants["context_fields"]
+    templates.env.globals["vendors"] = constants["vendors"]
+    templates.env.globals["application"] = "Overload Web"
+    return templates
+
+
 app = FastAPI(lifespan=lifespan)
 
-app.state.templates = config.get_templates()
+app.state.templates = get_templates()
 app.mount("/static", StaticFiles(directory=BASE_DIR / "static"), name="static")
 
 app.include_router(frontend.frontend_router)
