391695101052e962c994acd95b9d2d2b5033a1c7
Charlotte Kostelic <charlottekostelic@gmail.com>
2026-04-20 13:06:24 -0400

    removed `from_file` function with deprecated pydantic methods

--- overload_web/presentation/routers/order_templates.py
+++ overload_web/presentation/routers/order_templates.py
@@ -7,7 +7,7 @@ from typing import Annotated, Any
 
 from fastapi import APIRouter, Depends, Form, Request
 from fastapi.responses import HTMLResponse
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel
 
 from overload_web.application.commands.order_template import (
     CreateOrderTemplate,
@@ -52,6 +52,61 @@ class TemplatePatchModel(BaseModel):
     secondary_matchpoint: str | None = None
     tertiary_matchpoint: str | None = None
 
+    @classmethod
+    def from_form(
+        self,
+        acquisition_type: str | None = Form(default=None),
+        agent: str | None = Form(default=None),
+        blanket_po: str | None = Form(default=None),
+        claim_code: str | None = Form(default=None),
+        country: str | None = Form(default=None),
+        format: str | None = Form(default=None),
+        internal_note: str | None = Form(default=None),
+        lang: str | None = Form(default=None),
+        material_form: str | None = Form(default=None),
+        name: str | None = Form(default=None),
+        order_code_1: str | None = Form(default=None),
+        order_code_2: str | None = Form(default=None),
+        order_code_3: str | None = Form(default=None),
+        order_code_4: str | None = Form(default=None),
+        order_note: str | None = Form(default=None),
+        order_type: str | None = Form(default=None),
+        receive_action: str | None = Form(default=None),
+        selector_note: str | None = Form(default=None),
+        vendor_code: str | None = Form(default=None),
+        vendor_notes: str | None = Form(default=None),
+        vendor_title_no: str | None = Form(default=None),
+        primary_matchpoint: str | None = Form(default=None),
+        secondary_matchpoint: str | None = Form(default=None),
+        tertiary_matchpoint: str | None = Form(default=None),
+    ) -> TemplatePatchModel:
+        return TemplatePatchModel(
+            acquisition_type=acquisition_type,
+            agent=agent,
+            blanket_po=blanket_po,
+            claim_code=claim_code,
+            country=country,
+            format=format,
+            internal_note=internal_note,
+            lang=lang,
+            material_form=material_form,
+            name=name,
+            order_code_1=order_code_1,
+            order_code_2=order_code_2,
+            order_code_3=order_code_3,
+            order_code_4=order_code_4,
+            order_note=order_note,
+            order_type=order_type,
+            receive_action=receive_action,
+            selector_note=selector_note,
+            vendor_code=vendor_code,
+            vendor_notes=vendor_notes,
+            vendor_title_no=vendor_title_no,
+            primary_matchpoint=primary_matchpoint,
+            secondary_matchpoint=secondary_matchpoint,
+            tertiary_matchpoint=tertiary_matchpoint,
+        )
+
 
 class TemplateCreateModel(TemplatePatchModel):
     """
@@ -63,14 +118,6 @@ class TemplateCreateModel(TemplatePatchModel):
     agent: str
     primary_matchpoint: str
 
-    @field_validator("*", mode="before")
-    @classmethod
-    def parse_form_fields(cls, value: str) -> str | None:
-        if not value or value.strip() == "":
-            return None
-        else:
-            return value.strip()
-
 
 api_router = APIRouter()
 
@@ -78,7 +125,7 @@ api_router = APIRouter()
 @api_router.post("/template", response_class=HTMLResponse)
 def create_template(
     request: Request,
-    template: Annotated[Any, Depends(deps.from_form(TemplateCreateModel))],
+    template: Annotated[Any, Depends(TemplateCreateModel.from_form)],
     repository: Annotated[Any, Depends(deps.order_template_db)],
 ) -> HTMLResponse:
     """
@@ -157,7 +204,7 @@ def get_template_list(
 def update_template(
     request: Request,
     template_id: Annotated[str, Form(...)],
-    template_patch: Annotated[Any, Depends(deps.from_form(TemplatePatchModel))],
+    template_patch: Annotated[Any, Depends(TemplatePatchModel.from_form)],
     repository: Annotated[Any, Depends(deps.order_template_db)],
 ) -> HTMLResponse:
     """
--- overload_web/presentation/routers/pvf.py
+++ overload_web/presentation/routers/pvf.py
@@ -27,6 +27,19 @@ class MatchpointSchema(BaseModel):
     secondary_matchpoint: str | None = None
     tertiary_matchpoint: str | None = None
 
+    @classmethod
+    def from_form(
+        self,
+        primary_matchpoint: str | None = Form(default=None),
+        secondary_matchpoint: str | None = Form(default=None),
+        tertiary_matchpoint: str | None = Form(default=None),
+    ) -> MatchpointSchema:
+        return MatchpointSchema(
+            primary_matchpoint=primary_matchpoint,
+            secondary_matchpoint=secondary_matchpoint,
+            tertiary_matchpoint=tertiary_matchpoint,
+        )
+
 
 class TemplateDataModel(BaseModel):
     """Pydantic model for serializing/deserializing order template data"""
@@ -51,6 +64,51 @@ class TemplateDataModel(BaseModel):
     vendor_notes: str | None = None
     vendor_title_no: str | None = None
 
+    @classmethod
+    def from_form(
+        self,
+        acquisition_type: str | None = Form(default=None),
+        blanket_po: str | None = Form(default=None),
+        claim_code: str | None = Form(default=None),
+        country: str | None = Form(default=None),
+        format: str | None = Form(default=None),
+        internal_note: str | None = Form(default=None),
+        lang: str | None = Form(default=None),
+        material_form: str | None = Form(default=None),
+        order_code_1: str | None = Form(default=None),
+        order_code_2: str | None = Form(default=None),
+        order_code_3: str | None = Form(default=None),
+        order_code_4: str | None = Form(default=None),
+        order_note: str | None = Form(default=None),
+        order_type: str | None = Form(default=None),
+        receive_action: str | None = Form(default=None),
+        selector_note: str | None = Form(default=None),
+        vendor_code: str | None = Form(default=None),
+        vendor_notes: str | None = Form(default=None),
+        vendor_title_no: str | None = Form(default=None),
+    ) -> TemplateDataModel:
+        return TemplateDataModel(
+            acquisition_type=acquisition_type,
+            blanket_po=blanket_po,
+            claim_code=claim_code,
+            country=country,
+            format=format,
+            internal_note=internal_note,
+            lang=lang,
+            material_form=material_form,
+            order_code_1=order_code_1,
+            order_code_2=order_code_2,
+            order_code_3=order_code_3,
+            order_code_4=order_code_4,
+            order_note=order_note,
+            order_type=order_type,
+            receive_action=receive_action,
+            selector_note=selector_note,
+            vendor_code=vendor_code,
+            vendor_notes=vendor_notes,
+            vendor_title_no=vendor_title_no,
+        )
+
 
 api_router = APIRouter()
 
@@ -60,8 +118,8 @@ def process_acq_records(
     request: Request,
     fetcher: Annotated[Any, Depends(deps.get_fetcher)],
     marc_engine: Annotated[Any, Depends(deps.get_marc_engine)],
-    order_template: Annotated[Any, Depends(deps.from_form(TemplateDataModel))],
-    matchpoints: Annotated[Any, Depends(deps.from_form(MatchpointSchema))],
+    order_template: Annotated[Any, Depends(TemplateDataModel.from_form)],
+    matchpoints: Annotated[Any, Depends(MatchpointSchema.from_form)],
     repository: Annotated[Any, Depends(deps.pvf_batch_db)],
     local_files: Annotated[list | None, Form()] = None,
     remote_file_names: Annotated[list | None, Form()] = None,
@@ -162,8 +220,8 @@ def process_sel_records(
     request: Request,
     fetcher: Annotated[Any, Depends(deps.get_fetcher)],
     marc_engine: Annotated[Any, Depends(deps.get_marc_engine)],
-    order_template: Annotated[Any, Depends(deps.from_form(TemplateDataModel))],
-    matchpoints: Annotated[Any, Depends(deps.from_form(MatchpointSchema))],
+    order_template: Annotated[Any, Depends(TemplateDataModel.from_form)],
+    matchpoints: Annotated[Any, Depends(MatchpointSchema.from_form)],
     repository: Annotated[Any, Depends(deps.pvf_batch_db)],
     local_files: Annotated[list | None, Form()] = None,
     remote_file_names: Annotated[list | None, Form()] = None,
